6283a2fbf432a60b85f6f4fe278b95b1a46e98e2
Ben Woodard <woodard@redhat.com>
2026-08-11 15:57:44 -0700

    Make the test program more descriptive
    Prompt:
    
    Can you modify the test_tls app so that it also prints out the
    subtotal of static tls for each namespace and then sums them after
    iterating through all the namespaces that it has found?
    
    Signed-off-by: Ben Woodard <woodard@redhat.com>

--- test/test_tls_main.cpp
+++ test/test_tls_main.cpp
@@ -43,6 +43,8 @@ int main() {
     }
     
     int ns_id = 0;
+    size_t grand_total_tls = 0;
+    
     while (ext_debug != nullptr) {
         struct link_map* lmap = ext_debug->base.r_map;
         
@@ -52,6 +54,8 @@ int main() {
         if (lmap) {
             std::cout << "[main] Discovered Namespace " << ns_id << std::endl;
         }
+
+        size_t ns_total_tls = 0;
 
         while (lmap) {
             const char* name = (lmap->l_name && lmap->l_name[0]) ? lmap->l_name : "[main executable]";
@@ -84,8 +88,9 @@ int main() {
                 // Iterate the headers looking for the TLS segment
                 for (size_t i = 0; i < phnum; i++) {
                     if (phdr[i].p_type == PT_TLS) {
-                        std::cout << "[main] Namespace " << ns_id << " | " << base_name 
+                        std::cout << "[main]   -> " << base_name 
                                   << " TLS segment size: " << phdr[i].p_memsz << " bytes." << std::endl;
+                        ns_total_tls += phdr[i].p_memsz;
                         break;
                     }
                 }
@@ -94,12 +99,19 @@ int main() {
             lmap = lmap->l_next;
         }
         
+        std::cout << "[main] Subtotal for Namespace " << ns_id << ": " << ns_total_tls << " bytes." << std::endl;
+        grand_total_tls += ns_total_tls;
+
         // If glibc doesn't support namespace iteration, stop here
         if (ext_debug->base.r_version < 2) break; 
         
         ext_debug = ext_debug->r_next;
         ns_id++;
     }
+    
+    std::cout << "[main] ===============================================" << std::endl;
+    std::cout << "[main] Grand Total TLS across all namespaces: " << grand_total_tls << " bytes." << std::endl;
+    std::cout << "[main] ===============================================" << std::endl;
     
     return 0;
 }
